545caa4a5df0a570a03e55b5313927215c2b3dfd
Wiktoria <wikidryja@gmail.com>
2026-03-19 18:48:20 +0100

    Dodanie info o autorze w Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start projektu PGO");
+        System.out.print("Wersja 1.0");
+        System.out.print("Autor: Wiktoria")
     }
 }
